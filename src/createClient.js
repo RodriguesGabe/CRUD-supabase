8d99e6079f78156c89a0ecb4678d8d5088939cf5
Gitgabe <cybergabe01@gmail.com>
2023-11-18 21:28:40 -0300

    added added the folders src and public

---
+++ src/createClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase= createClient( 
+    "https://jiabaffahbfaouruqvvf.supabase.co", 
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppYWJhZmZhaGJmYW91cnVxdnZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAxNjc2NjUsImV4cCI6MjAxNTc0MzY2NX0.r6iVuXN8WXFVILU27c75HYuK6FZ3-HQGd8345tPYxow"
+) 
